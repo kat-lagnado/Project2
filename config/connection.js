088eb57f49b5ef5eec82df4a1e0f9929beca02cc
Electus <electuspro@gmail.com>
2019-06-22 13:38:42 -0400

    Working Post request

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,10 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "password",
-    database: "rdfiovyv7suq27i5"
+    database: "outfits_db"
   });
 }
 
